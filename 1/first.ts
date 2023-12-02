292bd8d9cd077b587d08c8682c83f4b250f41315
JLevente230 <jlevente230@gmail.com>
2023-12-02 13:43:11 +0100

    Day 1 done

--- 1/first.ts
+++ 1/first.ts
@@ -13,11 +13,37 @@ function isNumber(char: string) {
         char === '9';
 }
 
+function containsSpelled(text: string, index: number, forward: boolean) {
+    forward ? text = text.substring(0, index) : text = text.substring(index, text.length);
+
+    if (text.includes('one')) {
+        return '1';
+    } else if (text.includes('two')) {
+        return '2';
+    } else if (text.includes('three')) {
+        return '3';
+    } else if (text.includes('four')) {
+        return '4';
+    } else if (text.includes('five')) {
+        return '5';
+    } else if (text.includes('six')) {
+        return '6';
+    } else if (text.includes('seven')) {
+        return '7';
+    } else if (text.includes('eight')) {
+        return '8';
+    } else if (text.includes('nine')) {
+        return '9';
+    }
+
+    return 'nope';
+}
+
 let input = fs.readFileSync('1/input.txt', 'utf-8');
 let splitInput = input.split('\n');
 
 let result = 0;
-for (let i = 0; i < splitInput.length; i++ ) {
+for (let i = 0; i < splitInput.length; i++) {
     let first = '';
     let second = '';
     for (let j = 0; j < splitInput[i].length; j++) {
@@ -27,10 +53,17 @@ for (let i = 0; i < splitInput.length; i++ ) {
         if (isNumber(splitInput[i][splitInput[i].length - 1 - j]) && second === '') {
             second = splitInput[i][splitInput[i].length - 1 - j];
         }
-        if(first !== '' && second !== '') {
+        if (containsSpelled(splitInput[i], j + 1, true) !== 'nope' && first === '') {
+            first = containsSpelled(splitInput[i], j + 1, true);
+        }
+        if (containsSpelled(splitInput[i], splitInput[i].length - 1 - j, false) !== 'nope' && second === '') {
+            second = containsSpelled(splitInput[i], splitInput[i].length - 1 - j, false);
+        }
+        if (first !== '' && second !== '') {
             break;
         }
     }
+
     result += +(first + second);
 }
 
