819156fa04fdd7dec93253dd6ab25ba68e28cf60
JLevente230 <jlevente230@gmail.com>
2023-12-13 18:14:00 +0100

    Day 11.1 done

--- 11/eleventh.ts
+++ 11/eleventh.ts
@@ -1,15 +1,6 @@
 import * as fs from 'fs';
 
-let input = '...#......\n' +
-    '.......#..\n' +
-    '#.........\n' +
-    '..........\n' +
-    '......#...\n' +
-    '.#........\n' +
-    '.........#\n' +
-    '..........\n' +
-    '.......#..\n' +
-    '#...#.....';//fs.readFileSync('11/input.txt', 'utf-8');
+let input = fs.readFileSync('11/input.txt', 'utf-8');
 
 while (input.includes('\r')) input = input.replace('\r', '');
 
@@ -23,7 +14,6 @@ for (let i = 0; i < inputArray.length; i++) {
 let transposedMatrix: string[][] = inputMatrix.map(function (row) {
     return row.slice();
 });
-
 
 // this code is by Rajput-Ji
 for (let i = 0; i < transposedMatrix.length / 2; i++) {
@@ -43,31 +33,59 @@ for (let i = 0; i < transposedMatrix.length; i++) {
     }
 }
 
-for (let i = 0; i < inputMatrix.length; i++) {
+for (let i = 0; i < inputMatrix.length;) {
     if (!inputMatrix[i].includes('#')) {
         let inputFirst = inputMatrix.slice(0, i);
         let row: string[] = [];
         for (let j = 0; j < inputMatrix[i].length; j++) {
             row.push('.');
         }
-        inputFirst.push(row);
+        for (let k = 0; k < 1000000 - 1; k++) {
+            inputFirst.push(row);
+        }
         let inputSecond = inputMatrix.slice(i);
 
         inputMatrix = inputFirst.concat(inputSecond);
-        i = i + 2;
-    }
+        i = i + 1000000;
+    } else i++;
 }
 
 for (let i = 0; i < inputMatrix.length; i++) {
-    for (let j = 0; j < inputMatrix[i].length; j++) {
-        if (columns.includes(j)) {
-            let inputFirst = inputMatrix[i].slice(0, j);
-            let value = '.';
-            inputFirst.push(value);
-            let inputSecond = inputMatrix[i].slice(j);
-            inputMatrix[i] = inputFirst.concat(inputSecond);
+    let offset = 0;
+    for (let j = 0; j < inputMatrix[i].length;) {
+        if (columns.includes(j - offset)) {
+            let firstPart = inputMatrix[i].slice(0, j);
+            let secondPart = inputMatrix[i].slice(j);
+            for (let k = 0; k < 1000000 - 1; k++) {
+                firstPart.push('.');
+            }
+            inputMatrix[i] = firstPart.concat(secondPart);
+            j = j + 1000000;
+            offset = offset + 1000000;
+        } else {
             j++;
         }
     }
 }
 
+let tempArray = [];
+for (let i = 0; i < inputMatrix.length; i++) {
+    tempArray.push(inputMatrix[i].join(''));
+}
+
+let galaxies: number[][] = [];
+
+for (let i = 0; i < inputMatrix.length; i++) {
+    for (let j = 0; j < inputMatrix[i].length; j++) {
+        if (inputMatrix[i][j] === '#') galaxies.push([i, j]);
+    }
+}
+
+let result = 0;
+for (let i = 0; i < galaxies.length - 1; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+        result += (Math.abs((galaxies[j][1] - galaxies[i][1])) + Math.abs((galaxies[j][0] - galaxies[i][0])));
+    }
+}
+
+console.log(result)
