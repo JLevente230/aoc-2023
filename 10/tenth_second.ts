3060405c76cdd1b7cb84c6d6c9e0cde1e6968f54
JLevente230 <jlevente230@gmail.com>
2023-12-14 14:21:45 +0100

    Day 10.2 works for small input

--- 10/tenth_second.ts
+++ 10/tenth_second.ts
@@ -122,106 +122,85 @@ while (true) {
     steps++;
 }
 
-function noWayOut(indexI: number, indexJ: number) {
-    let x = indexI;
-    let y = indexJ;
-    while (x > 0) {
-        if (outPutMatrix[x][indexJ] !== '.') break;
-        x--;
-    }
-    if (x === 0) return false;
-    x = indexI;
-    while (x < outPutMatrix.length - 1) {
-        if (outPutMatrix[x][indexJ] !== '.') break;
-        x++;
-    }
-    if (x === outPutMatrix.length - 1) return false;
-
-    while (y > 0) {
-        if (outPutMatrix[indexI][y] !== '.') break;
-        y--;
-    }
-    if (y === 0) return false;
-    y = indexJ;
-    while (y < outPutMatrix[0].length - 1) {
-        if (outPutMatrix[indexI][y] !== '.') break;
-        y++;
-    }
-    if (y === outPutMatrix[0].length - 1) return false;
-
-    return true;
-}
-
-function noWayOutExpanded(indexI: number, indexJ: number) {
-    let x = indexI;
-    let y = indexJ;
-    while (x > 0) {
-        if (expandedMatrix[x][indexJ] !== '.') break;
-        x--;
-    }
-    if (x === 0) return false;
-    x = indexI;
-    while (x < expandedMatrix.length - 1) {
-        if (expandedMatrix[x][indexJ] !== '.') break;
-        x++;
-    }
-    if (x === expandedMatrix.length - 1) return false;
-
-    while (y > 0) {
-        if (expandedMatrix[indexI][y] !== '.') break;
-        y--;
-    }
-    if (y === 0) return false;
-    y = indexJ;
-    while (y < expandedMatrix[0].length - 1) {
-        if (expandedMatrix[indexI][y] !== '.') break;
-        y++;
-    }
-    if (y === expandedMatrix[0].length - 1) return false;
-
-    return true;
-}
-
-let potentiallyCovered: number[][] = [];
-for (let i = 0; i < outPutMatrix.length; i++) {
-    for (let j = 0; j < outPutMatrix[i].length; j++) {
-        if (outPutMatrix[i][j] === '.' && noWayOut(i, j)) {
-            outPutMatrix[i][j] = '.';
-            potentiallyCovered.push([i, j]);
-        }
-    }
-}
-
 let expandedMatrix: string[][] = [];
-for (let i = 0; i < (outPutMatrix.length * 2); i++) {
+for (let i = 0; i < outPutMatrix.length * 2; i++) {
     expandedMatrix.push([]);
-    for (let j = 0; j < (outPutMatrix[0].length * 2); j++) {
-        expandedMatrix[i].push('');
-    }
-}
-
-for (let i = 0; i < expandedMatrix.length; i++) {
-    for (let j = 0; j < expandedMatrix[i].length; j++) {
-        if (outPutMatrix[Math.floor(i / 2)][Math.floor(j / 2)] === '-' ||
-            outPutMatrix[Math.floor(i / 2)][Math.floor(j / 2)] === 'L' ||
-            outPutMatrix[Math.floor(i / 2)][Math.floor(j / 2)] === 'F') {
-            expandedMatrix[i][j] = outPutMatrix[Math.floor(i / 2)][Math.floor(j / 2)];
-            expandedMatrix[i][j + 1] = '-';
+    for (let j = 0; j < outPutMatrix[0].length * 2; j++) {
+        if (i % 2 === 0 && j % 2 === 0) {
+            expandedMatrix[i][j] = outPutMatrix[i / 2][j / 2];
         } else {
-            expandedMatrix[i][j] = outPutMatrix[Math.floor(i / 2)][Math.floor(j / 2)];
-            expandedMatrix[i][j + 1] = '.';
+            expandedMatrix[i][j] = '.';
         }
-        j++;
+    }
+}
+
+let foundS = false;
+for (let i = 0; i < expandedMatrix.length && !foundS; i++) {
+    for (let j = 0; j < expandedMatrix[i].length; j++) {
+        if (expandedMatrix[i][j] === 'S') {
+            expandedMatrix[i][j] = 'F'; // REWRITE THIS
+            foundS = true;
+            break;
+        }
     }
 }
 
 for (let i = 0; i < expandedMatrix.length; i++) {
     for (let j = 0; j < expandedMatrix[i].length; j++) {
-        if (expandedMatrix[i][j] === '.' && noWayOutExpanded(i, j)) {
-            expandedMatrix[i][j] = '0';
+        if (i < expandedMatrix.length - 1 &&
+            (expandedMatrix[i][j] === '|' ||
+                expandedMatrix[i][j] === '7')) {
+            if (expandedMatrix[i + 1][j] === '.') expandedMatrix[i + 1][j] = '|';
+        } else if (j < expandedMatrix[i].length - 1 &&
+            (expandedMatrix[i][j] === '-' ||
+                expandedMatrix[i][j] === 'L')) {
+            if (expandedMatrix[i][j + 1] === '.') expandedMatrix[i][j + 1] = '-';
+        } else if ((j < expandedMatrix[i].length - 1 && i < expandedMatrix.length - 1) &&
+            expandedMatrix[i][j] === 'F') {
+            if (expandedMatrix[i][j + 1] === '.') expandedMatrix[i][j + 1] = '-';
+            if (expandedMatrix[i + 1][j] === '.') expandedMatrix[i + 1][j] = '|';
         }
     }
 }
+
+let startingI = -1;
+let startingJ = -1;
+for (let i = 0; i < expandedMatrix.length && startingI === -1; i++) {
+    for (let j = 0; j < expandedMatrix[i].length; j++) {
+        if (expandedMatrix[i][j] === '.') {
+            startingI = i;
+            startingJ = j;
+            break;
+        }
+    }
+}
+
+function infect(indexI: number, indexJ: number) {
+    expandedMatrix[indexI][indexJ] = 'o';
+
+    if (indexJ < expandedMatrix[0].length - 1) {
+        if (expandedMatrix[indexI][indexJ + 1] === '.') {
+            infect(indexI, indexJ + 1);
+        }
+    }
+    if (indexJ > 0) {
+        if (expandedMatrix[indexI][indexJ - 1] === '.') {
+            infect(indexI, indexJ - 1);
+        }
+    }
+    if (indexI < expandedMatrix.length - 1) {
+        if (expandedMatrix[indexI + 1][indexJ] === '.') {
+            infect(indexI + 1, indexJ);
+        }
+    }
+    if (indexI > 0) {
+        if (expandedMatrix[indexI - 1][indexJ] === '.') {
+            infect(indexI - 1, indexJ);
+        }
+    }
+}
+
+infect(startingI, startingJ);
 
 let realOutPutMatrix: string[] = [];
 for (let i = 0; i < expandedMatrix.length; i++) {
