468bb346e0dfc490966866b7627e513ee3d297ce
JLevente230 <jlevente230@gmail.com>
2023-12-15 03:27:50 +0100

    Day 10.2 almost works

--- 10/tenth_second.ts
+++ 10/tenth_second.ts
@@ -1,14 +1,15 @@
 import * as fs from 'fs';
 
-let input = '..........\n' +
-    '.S------7.\n' +
-    '.|F----7|.\n' +
-    '.||OOOO||.\n' +
-    '.||OOOO||.\n' +
-    '.|L-7F-J|.\n' +
-    '.|II||II|.\n' +
-    '.L--JL--J.\n' +
-    '..........';//fs.readFileSync('10/input.txt', 'utf-8');
+let input = '.F----7F7F7F7F-7....\n' +
+    '.|F--7||||||||FJ....\n' +
+    '.||.FJ||||||||L7....\n' +
+    'FJL7L7LJLJ||LJ.L-7..\n' +
+    'L--J.L7...LJS7F-7L7.\n' +
+    '....F-J..F7FJ|L7L7L7\n' +
+    '....L7.F7||L7|.L7L7|\n' +
+    '.....|FJLJ|FJ|F7|.LJ\n' +
+    '....FJL-7.||.||||...\n' +
+    '....L---J.LJ.LJLJ...';//fs.readFileSync('10/input.txt', 'utf-8');
 
 while (input.includes('\r')) input = input.replace('\r', '');
 
@@ -165,42 +166,143 @@ for (let i = 0; i < expandedMatrix.length; i++) {
 
 let startingI = -1;
 let startingJ = -1;
-for (let i = 0; i < expandedMatrix.length && startingI === -1; i++) {
-    for (let j = 0; j < expandedMatrix[i].length; j++) {
-        if (expandedMatrix[i][j] === '.') {
-            startingI = i;
-            startingJ = j;
-            break;
-        }
-    }
-}
 
-function infect(indexI: number, indexJ: number) {
+function infect(indexI: number, indexJ: number, count: number) {
     expandedMatrix[indexI][indexJ] = 'o';
+    areas[count].push([indexI, indexJ]);
 
     if (indexJ < expandedMatrix[0].length - 1) {
         if (expandedMatrix[indexI][indexJ + 1] === '.') {
-            infect(indexI, indexJ + 1);
+            infect(indexI, indexJ + 1, count);
         }
     }
     if (indexJ > 0) {
         if (expandedMatrix[indexI][indexJ - 1] === '.') {
-            infect(indexI, indexJ - 1);
+            infect(indexI, indexJ - 1, count);
         }
     }
     if (indexI < expandedMatrix.length - 1) {
         if (expandedMatrix[indexI + 1][indexJ] === '.') {
-            infect(indexI + 1, indexJ);
+            infect(indexI + 1, indexJ, count);
         }
     }
     if (indexI > 0) {
         if (expandedMatrix[indexI - 1][indexJ] === '.') {
-            infect(indexI - 1, indexJ);
+            infect(indexI - 1, indexJ, count);
         }
     }
 }
 
-infect(startingI, startingJ);
+let areas: number[][][] = [];
+let count = 0;
+while (true) {
+    for (let i = 0; i < expandedMatrix.length && startingI === -1; i++) {
+        for (let j = 0; j < expandedMatrix[i].length; j++) {
+            if (expandedMatrix[i][j] === '.') {
+                startingI = i;
+                startingJ = j;
+                break;
+            }
+        }
+    }
+
+    if (startingI === -1) break;
+    areas.push([]);
+    infect(startingI, startingJ, count);
+    count++;
+    startingI = -1;
+    startingJ = -1;
+}
+
+function wayOut(indexI: number, indexJ: number) {
+    let indexFirst = indexI;
+    let indexSecond = indexJ;
+    while (indexFirst >= 0) {
+        if (expandedMatrix[indexFirst][indexJ] !== 'o') {
+            break;
+        }
+        indexFirst--;
+    }
+
+    if (indexFirst <= 0) return true;
+
+    indexFirst = indexI;
+
+    while (indexFirst <= expandedMatrix.length - 1) {
+        if (expandedMatrix[indexFirst][indexJ] !== 'o') {
+            break;
+        }
+        indexFirst++;
+    }
+    if (indexFirst >= expandedMatrix.length - 1) return true;
+
+    while (indexSecond >= 0) {
+        if (expandedMatrix[indexI][indexSecond] !== 'o') {
+            break;
+        }
+        indexSecond--;
+    }
+    if (indexSecond <= 0) return true;
+
+    indexSecond = indexJ;
+
+    while (indexSecond <= expandedMatrix[0].length - 1) {
+        if (expandedMatrix[indexI][indexSecond] !== 'o') {
+            break;
+        }
+        indexSecond++;
+    }
+    if (indexSecond >= expandedMatrix[0].length - 1) return true;
+    return false;
+}
+
+let coveredAreas: number[][] = [];
+let thereIsAWayOut = false;
+for (let i = 0; i < areas.length; i++) {
+    for (let j = 0; j < areas[i].length; j++) {
+        if (wayOut(areas[i][j][0], areas[i][j][1])) {
+            thereIsAWayOut = true;
+            break;
+        }
+    }
+    if (!thereIsAWayOut) {
+        for (let k = 0; k < areas[i].length; k++) {
+            coveredAreas.push(areas[i][k]);
+        }
+    }
+    thereIsAWayOut = false;
+}
+
+function coveredIncludes(indexI: number, indexJ: number): boolean {
+    for (let i = 0; i < coveredAreas.length; i++) {
+        if (coveredAreas[i][0] === indexI && coveredAreas[i][1] === indexJ) {
+            return true;
+        }
+    }
+    return false;
+}
+
+for (let i = 0; i < expandedMatrix.length; i++) {
+    for (let j = 0; j < expandedMatrix[i].length; j++) {
+        if (expandedMatrix[i][j] === 'o' && coveredIncludes(i, j)) {
+            expandedMatrix[i][j] = 'X';
+        }
+    }
+}
+
+console.log(areas)
+
+let result = 0;
+for (let i = 0; i < expandedMatrix.length; i++) {
+    for (let j = 0; j < expandedMatrix[i].length; j++) {
+        if (i < expandedMatrix.length - 1 && j < expandedMatrix[i].length - 1 &&
+            expandedMatrix[i + 1][j] === 'X' && expandedMatrix[i][j] === 'X' &&
+            expandedMatrix[i][j + 1] === 'X' && expandedMatrix[i + 1][j + 1] === 'X') {
+            result++;
+            j++;
+        }
+    }
+}
 
 let realOutPutMatrix: string[] = [];
 for (let i = 0; i < expandedMatrix.length; i++) {
@@ -212,3 +314,4 @@ for (let i = 0; i < expandedMatrix.length; i++) {
 }
 
 console.log(realOutPutMatrix);
+console.log(result / 2);
